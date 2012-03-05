aa687ef7076f6ad7fb53e278435ae62feed1d021
Guilcher Pascal <pgu.dev@gmail.com>
2012-03-05 22:50:00 +0100

    ..

--- src/com/pgu/calendar/server/CheckPillServlet.java
+++ src/com/pgu/calendar/server/CheckPillServlet.java
@@ -11,6 +11,7 @@ import java.util.logging.Logger;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
@@ -89,22 +90,57 @@ public class CheckPillServlet extends HttpServlet {
         final Session session = Session.getDefaultInstance(props);
 
         final Message msg = new MimeMessage(session);
+        setFrom(msg);
+        setTo(msg);
+        setSubject("Remember :-)", msg);
+        setBody(isMorning, msg);
+        sendEmail(msg);
+    }
+
+    private void sendEmail(final Message msg) {
         try {
-            msg.setFrom(new InternetAddress(""));
+            Transport.send(msg);
+        } catch (final MessagingException e) {
+            LOGGER.severe(e.getMessage());
+        }
+    }
+
+    private void setFrom(final Message msg) {
+        try {
+            msg.setFrom(new InternetAddress("guilcher.pascal@gmail.com", "Pill Admin"));
         } catch (final AddressException e) {
             LOGGER.severe(e.getMessage());
         } catch (final MessagingException e) {
             LOGGER.severe(e.getMessage());
+        } catch (final UnsupportedEncodingException e) {
+            LOGGER.severe(e.getMessage());
         }
+    }
 
+    private void setTo(final Message msg) {
         try {
-            msg.addRecipient(Message.RecipientType.TO, new InternetAddress("", "Eemm"));
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress("emnite@gmail.com", "Eemm"));
         } catch (final UnsupportedEncodingException e) {
             LOGGER.severe(e.getMessage());
         } catch (final MessagingException e) {
             LOGGER.severe(e.getMessage());
         }
-        final String msgBody = "You need to take you pill of the " + (isMorning ? "morning" : "afternoon");
+    }
+
+    private void setBody(final boolean isMorning, final Message msg) {
+        try {
+            msg.setText(String.format("You need to take your %s pill.", isMorning ? "morning" : "evening"));
+        } catch (final MessagingException e) {
+            LOGGER.severe(e.getMessage());
+        }
+    }
+
+    private void setSubject(final String subject, final Message msg) {
+        try {
+            msg.setSubject(subject);
+        } catch (final MessagingException e) {
+            LOGGER.severe(e.getMessage());
+        }
     }
 
 }
